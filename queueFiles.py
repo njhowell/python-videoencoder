d69f5ad5f64d88bb8a1820260bb756b826ba76d1
Nick Howell <nick@nickhowell.co.uk>
2018-02-01 18:47:45 +0000

    check for files and add them to the queue to process

---
+++ queueFiles.py
@@ -0,0 +1,17 @@
+import shutil
+from os import listdir
+from os.path  import isfile, join
+from os import walk
+import videoTasks
+from dirConfig import srcDir, encDir, outDir
+
+
+f = []
+for (dirpath, dirnames, filenames) in walk (srcDir):
+	print filenames
+	f.extend(filenames)
+
+for file in f:
+	shutil.move(srcDir+file, encDir+file)
+	srcFile = encDir+file
+	videoTasks.encode.delay(srcFile, outDir)
