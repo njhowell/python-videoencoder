fb65f09686e709e86d01af5aabcc414ce3ad1327
Nick Howell <nick@nickhowell.co.uk>
2018-02-13 19:03:03 +0000

    Some of the videos hit this most excellent error https://trac.ffmpeg.org/ticket/6375. Apparently setting the max muxing queue to a larger number helps :shrugs:

--- videoTasks.py
+++ videoTasks.py
@@ -9,6 +9,6 @@ app.config_from_object('celeryconfig')
 @app.task
 def encode(srcFile, dstFolder):
     outFileName = dstFolder+os.path.splitext(os.path.basename(srcFile))[0]+".mkv"
-    argsStr =  " -i \"%s\" -map 0 -c copy -c:v libx264 -crf 20 -y \"%s\" " % (srcFile, outFileName)
+    argsStr =  " -i \"%s\" -max_muxing_queue_size 1000 -map 0 -c copy -c:v libx264 -crf 20 -y \"%s\" " % (srcFile, outFileName)
     result = call("ffmpeg" + argsStr, shell=True)
     return result
